7c425f301d41b847bca54a9e461df722003431ac
jjoao07 <jjoao@besidecod.com>
2017-07-28 15:09:39 +0200

    Finished the project
    Just Finished the project with with full crud, create, update delete
    same as update son file

---
+++ resources/views/products/edit.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.modal')
+
+@section('content')
+
+<div class="modal-dialog">
+
+    <div class="modal-content">
+        <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+            <h5 class="modal-title">Edit Product</h5>
+        </div>
+ 
+        {!! Form::model($product, ['route' => ['products.update',$product->id], 'method'=>'PATCH', 'class'=>"ajax-submit"]) !!}
+
+        <div class="modal-body">
+        @include('products.partials._form')
+        
+        <div class="modal-footer">
+            <button type="submit" data-rel="tooltip" data-placement="top" title="Save" class="btn btn-xs btn-success disabled" style="pointer-events: all; cursor: pointer;"><i class="fa fa-save"></i> Save</button>
+        
+            <button type="button" data-rel="tooltip" data-placement="top" title="Close" data-dismiss="modal" class="btn btn-xs btn-danger">
+         <i class="fa fa-times"></i> Close</button>
+        </div>
+
+        {!! Form::close() !!}
+        </div>
+    </div>
+</div>
+@endsection
